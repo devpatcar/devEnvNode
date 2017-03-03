0f8aff7d84f8fef881fd2337866230e37fd94355
Patrik Carlsson <Patrik Carlsson>
2017-03-03 22:02:03 +0100

    now working

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import {getUsers} from './api/userApi';
+import './index.css';
+import {getUsers, deleteUser} from './api/userApi';
 
 // Populate table of users via API call.
 getUsers().then(result => {
-    let userBody = "";
+    let usersBody = "";
 
     result.forEach(user => {       
-        userBody += '<tr><td><a href="#" data-id="'+user.id+'" class="deleteUser">Delete</a></td><td>'+user.id+'</td><td>'+user.firstName+'</td><td>'+user.lastName+'</td><td>'+user.email+'</td></tr>'
-            
+        usersBody += `<tr>
+        <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+        <td>${user.id}</td><td>${user.firstName}</td><td>${user.lastName}</td>
+        <td>${user.email}</td>
+        </tr>`
+
     });
 
-    global.document.getElementById("users").innerHtml = userBody;
-    global.document.getElementById("test").innerHtml = "test";
+    $('tbody').html(usersBody);  
+
+    const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+    // Must use array.from to create a real array from a DOM collection
+    // getElementsByClassName only returns an "array like" object
+    Array.from(deleteLinks, link => {
+        link.onclick = function(event) {
+            const element = event.target;
+            event.preventDefault();
+            deleteUser(element.attributes["data-id"].value);
+            const row = element.parentNode.parentNode;
+            row.parentNode.removeChild(row);
+        }
+    });
 });
